016e3b8eb88b741cb4faf713912a0c02b7f9954e
Shreshth Mohan <sremog@gmail.com>
2022-03-07 21:38:00 +0530

    remove tsout dir

--- tsout/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function roundedPolygonBySideLength({ sideLength, sideCount, borderRadius, cx, cy, }: {
-    sideLength: number;
-    sideCount: number;
-    borderRadius?: number;
-    cx?: number;
-    cy?: number;
-}): string;
